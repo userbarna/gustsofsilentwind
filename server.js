9883b7eea0dc1c82e11552083e4f6ba34d1d4e5d
sdeveloper <dearclosercash@gmail.com>
2013-01-17 14:05:06 +0100

    Added EMAILJS functionality. Gmail test account created

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ var express = require('express'),
     port = process.env.PORT || 5000,
     app = express(),
     hbs = require('hbs'),
-    fs = require('fs');
+    fs = require('fs'),
+    email = require('emailjs'),
+    server = email.server.connect({
+      user: "goswtestemail@gmail.com",
+      password: "goswtestemail111111",
+      host: "smtp.gmail.com",
+      ssl:  true
+    });
 
 app.configure(function(){
     app.set('views',__dirname + '/app/views');
@@ -27,18 +34,47 @@ app.get('*', function (req, res, next) {
     //res.locals.videoId = req.params.id;
     res.render("cover");
 });*/
+var contador = 0; 
 
 app.get('/', function(req, res){
     var partial = fs.readFileSync(__dirname + '/app/views/calmly.html', 'utf8');
     hbs.registerPartial('content', partial);
     //res.locals.videoId = req.params.id;
-    res.render("calmly");
+    res.render("calmly");    
+
+    //Send message. Callback tells us whether there's been an error or not
+    server.send({
+      text: "Visita numero: " + contador++ + " " + getClientIp(req),
+      from: "gustsofsilentwind@gmail.com",
+      to: "gustsofsilentwind@gmail.com",
+      cc: "",
+      subject: contador++ + " " + getClientIp(req)
+    }, function(err,message){ console.log(err || message);}); 
 });
+
+function getClientIp(req) {
+  var ipAddress;
+  // Amazon EC2 / Heroku workaround to get real client IP
+  var forwardedIpsStr = req.header('x-forwarded-for'); 
+  if (forwardedIpsStr) {
+    // 'x-forwarded-for' header may return multiple IP addresses in
+    // the format: "client IP, proxy 1 IP, proxy 2 IP" so take the
+    // the first one
+    var forwardedIps = forwardedIpsStr.split(',');
+    ipAddress = forwardedIps[0];
+  }
+  if (!ipAddress) {
+    // Ensure getting client IP address still works in
+    // development environment
+    ipAddress = req.connection.remoteAddress;
+  }
+  return ipAddress;
+};
+
 
 /*app.get('/dmca', function(req, res){
     var partial = fs.readFileSync(__dirname + '/app/views/dmca.html', 'utf8');
     hbs.registerPartial('content', partial);
     res.render("dmca");
-});*/
-
+});*/ 
 app.listen(port);
